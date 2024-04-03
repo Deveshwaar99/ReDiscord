60a36deb9bb3bb5deffd53e6806ac9d698d3d336
Deveshwaar <deveshwaar99@gmail.com>
2024-04-03 18:50:58 +0530

    feat(ui): Add light mode and dark mode support

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function AuthLayout({ children }: { children: React.ReactNode }) {
-  return <div className=" flex items-center justify-center h-full">{children}</div>
+  return <div className=" flex items-center justify-center h-full bg-white ">{children}</div>
 }
 
 export default AuthLayout
---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { ThemeSwitch } from '@/components/ThemeSwitch'
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-full ">
+      <h1 className="">Hello</h1>
+      <ThemeSwitch />
+    </div>
+  )
+}
