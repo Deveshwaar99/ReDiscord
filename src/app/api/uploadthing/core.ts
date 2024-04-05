942b1e8ba4fc26d2983bdc5cd6c706f9fd2c6d0a
Deveshwaar <deveshwaar99@gmail.com>
2024-04-05 22:00:27 +0530

    configure uploadthing

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,27 @@
+import { auth } from '@clerk/nextjs'
+import { createUploadthing, type FileRouter } from 'uploadthing/next'
+import { UploadThingError } from 'uploadthing/server'
+
+const f = createUploadthing()
+
+const handleAuthorizarion = () => {
+  const { userId } = auth()
+  if (!userId) throw new UploadThingError('Unauthorized')
+  return { userId }
+}
+
+export const ourFileRouter = {
+  serverImage: f({ image: { maxFileSize: '4MB', maxFileCount: 1 } })
+    .middleware(() => handleAuthorizarion())
+    .onUploadComplete(async ({ metadata, file }) => {
+      return { uploadedBy: metadata.userId }
+    }),
+
+  messageFile: f(['image', 'pdf', 'text', 'audio'])
+    .middleware(() => handleAuthorizarion())
+    .onUploadComplete(async ({ metadata, file }) => {
+      return { uploadedBy: metadata.userId }
+    }),
+} satisfies FileRouter
+
+export type OurFileRouter = typeof ourFileRouter
