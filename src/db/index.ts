ca85e9dc0c305fa2d21a941f8c5c0c825da2686e
Deveshwaar <deveshwaar99@gmail.com>
2024-04-06 22:22:09 +0530

    refactor: add schema to db connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,11 @@
 import { Pool } from '@neondatabase/serverless'
 import { drizzle } from 'drizzle-orm/neon-serverless'
+import * as schema from '@/db/schema'
 // let dbInstance: NeonHttpDatabase<Record<string, never>>
 
 // if (!dbInstance) {
 const pool = new Pool({ connectionString: process.env.DATABASE_URL })
-let dbInstance = drizzle(pool)
+let dbInstance = drizzle(pool, { schema })
 // }
 
 export { dbInstance as db }
