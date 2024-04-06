fb73a5b5152560d3adbb23a4e258c0486e2593de
Deveshwaar <deveshwaar99@gmail.com>
2024-04-06 22:28:25 +0530

    feat: add navigation sidebar

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,14 @@
 import { ThemeSwitch } from '@/components/ThemeSwitch'
+import CreateServerModal from '@/components/modals/CreateServer'
+import { initialProfile } from '@/lib/initialProfile'
 
-export default function Home() {
+export default async function Home() {
+  const profile = await initialProfile()
+  console.log(profile)
   return (
-    <div className="flex items-center justify-center h-full ">
+    <div className="flex h-full items-center justify-center">
       <h1 className="">Hello</h1>
-      <ThemeSwitch />
+      <CreateServerModal />
     </div>
   )
 }
--- src/components/navigation/NavigationSidebar.tsx
+++ src/components/navigation/NavigationSidebar.tsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import { Separator } from '@/components//ui/separator'
+import { ThemeSwitch } from '@/components/ThemeSwitch'
+import NavigationItem from '@/components/navigation/NavigationItem'
 import NavigiationAction from '@/components/navigation/NavigiationAction'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { db } from '@/db'
+import { getProfile } from '@/lib/getProfile'
+import { UserButton, redirectToSignIn } from '@clerk/nextjs'
 
-function NavigationSidebar() {
+async function NavigationSidebar() {
+  const profile = await getProfile()
+  if (!profile) {
+    return redirectToSignIn()
+  }
+  const servers = await db.query.Server.findMany({
+    with: {
+      members: {
+        where: (members, { eq }) => eq(members.profileId, profile.id),
+      },
+    },
+  })
+  console.log(servers)
+
   return (
     <div className="flex h-full w-full flex-col items-center space-y-4 bg-[#E3E5E8] py-3 text-primary dark:bg-[#1E1F22]">
       <NavigiationAction />
+      <Separator className="mx-auto h-[2px] w-10 rounded-md bg-zinc-300 dark:bg-zinc-700" />
+      <ScrollArea className="w-full flex-1">
+        {servers.map(server => (
+          <div key={server.id} className="mb-4">
+            <NavigationItem id={server.id} name={server.name} imageUrl={server.imageUrl} />
+          </div>
+        ))}
+      </ScrollArea>
+      <div className="mt-auto flex flex-col items-center gap-y-4 pb-3">
+        <ThemeSwitch />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: 'w-[44px] h-[44px]',
+            },
+          }}
+        />
+      </div>
     </div>
   )
 }
