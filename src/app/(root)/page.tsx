d4bccbc798fd03b4866c0f1b0f20216ea41c5570
Deveshwaar <deveshwaar99@gmail.com>
2024-04-06 22:41:55 +0530

    Add modal provider to layout

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,14 +1,11 @@
-import { ThemeSwitch } from '@/components/ThemeSwitch'
-import CreateServerModal from '@/components/modals/CreateServer'
 import { initialProfile } from '@/lib/initialProfile'
 
 export default async function Home() {
   const profile = await initialProfile()
-  console.log(profile)
+
   return (
     <div className="flex h-full items-center justify-center">
       <h1 className="">Hello</h1>
-      <CreateServerModal />
     </div>
   )
 }
