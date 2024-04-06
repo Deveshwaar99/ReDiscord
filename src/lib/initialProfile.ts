af1f7952a21ffe07e52537e87dd3b956da34e38a
Deveshwaar <deveshwaar99@gmail.com>
2024-04-06 22:29:38 +0530

    add profile to db

---
+++ src/lib/initialProfile.ts
@@ -0,0 +1,28 @@
+import { db } from '@/db'
+import { Profile, SelectProfile } from '@/db/schema'
+import { currentUser, redirectToSignIn } from '@clerk/nextjs'
+import { eq } from 'drizzle-orm'
+
+export async function initialProfile(): Promise<SelectProfile> {
+  const user = await currentUser()
+  if (!user) {
+    return redirectToSignIn()
+  }
+
+  const [profile] = await db.select().from(Profile).where(eq(Profile.clerkId, user.id))
+  if (profile) {
+    return profile
+  }
+
+  const [newProfile] = await db
+    .insert(Profile)
+    .values({
+      clerkId: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+      imageUrl: user.imageUrl,
+      email: user.emailAddresses[0].emailAddress,
+    })
+    .returning()
+
+  return newProfile
+}
