e8d90d636fcaebc32917bba7ee028c7b6bfc9cec
Deveshwaar <deveshwaar99@gmail.com>
2024-05-10 21:42:06 +0530

    fetch server and pass it to ServerSidebar

--- src/app/(root)/(routes)/servers/[serverId]/layout.tsx
+++ src/app/(root)/(routes)/servers/[serverId]/layout.tsx
@@ -5,6 +5,7 @@ import { getProfile } from '@/lib/getProfile'
 import { redirectToSignIn } from '@clerk/nextjs'
 import { eq } from 'drizzle-orm'
 import { redirect } from 'next/navigation'
+import { MemberRoles } from '../../../../../../types'
 
 async function ServerLayout({
   children,
@@ -20,15 +21,26 @@ async function ServerLayout({
 
   const server = await db.query.Server.findFirst({
     where: (Server, { eq, and, exists }) => and(eq(Server.id, params.serverId), exists(memberSql)),
-    with: { members: true },
+    with: {
+      channels: {
+        orderBy: (channels, { asc }) => [asc(channels.createdAt)],
+      },
+      members: {
+        with: {
+          profile: true,
+        },
+        orderBy: (members, { asc }) => [asc(members.role)],
+      },
+    },
   })
 
   if (!server) redirect('/')
 
+  const role = server.members.find(member => member.profileId === profile.id)?.role
   return (
     <div className="h-full">
       <div className="fixed inset-y-0 z-20 hidden h-full w-60 flex-col md:flex">
-        <ServerSidebar serverId={server.id} />
+        <ServerSidebar server={server} role={MemberRoles[role!]} />
       </div>
       <main className="h-full md:pl-60">{children}</main>
     </div>
