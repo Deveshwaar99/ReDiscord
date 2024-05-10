53e9ef491648db3893818a4285fd4ec59810dcd2
Deveshwaar <deveshwaar99@gmail.com>
2024-05-10 13:01:51 +0530

    add function to validate channel schema

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,22 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+import { z } from 'zod'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+const ChannelDataSchema = z.object({
+  name: z
+    .string()
+    .min(5)
+    .max(15)
+    .refine(val => val !== 'general', {
+      message: "The name cannot be 'general'",
+    }),
+  type: z.enum(['AUDIO', 'TEXT', 'VIDEO']),
+})
+export function verifyChannelData(requestData: any) {
+  const result = ChannelDataSchema.safeParse(requestData)
+  return result
 }
