08d973d68c0306b0b255105406e23f1a93ed1555
Deveshwaar <deveshwaar99@gmail.com>
2024-05-10 23:14:25 +0530

    feat(server-member): Add url on button click

--- src/components/server/ServerMember.tsx
+++ src/components/server/ServerMember.tsx
@@ -10,7 +10,7 @@ import UserAvatar from '../UserAvatar'
 type ServerMemberProps = {
   member: SelectMember & { profile: SelectProfile }
   server: SelectServer
-  role: MemberRoles | undefined
+  role: MemberRoles
 }
 
 const roleIconMap: { [key in MemberRoles]: LucideIcon | null } = {
@@ -25,8 +25,13 @@ function ServerMember({ member, server, role }: ServerMemberProps) {
 
   const Icon = roleIconMap[member.role]
 
+  const onClick = () => {
+    router.push(`/servers/${server.id}/conversations/${member.id}`)
+  }
+
   return (
     <button
+      onClick={onClick}
       className={cn(
         'group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1',
         params?.memberId === member.id ? 'bg-zinc-700/20 dark:bg-zinc-700' : null
