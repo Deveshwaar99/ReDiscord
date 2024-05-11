6ef9a0e7fe02f2550bd6d1497ad34705decbd4db
Deveshwaar <deveshwaar99@gmail.com>
2024-05-11 20:18:55 +0530

    refactor : Redirect to general CHannel page on requests without channelId

--- src/app/(root)/servers/[serverId]/page.tsx
+++ src/app/(root)/servers/[serverId]/page.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import { db } from '@/db'
+import { Channel } from '@/db/schema'
+import { currentUser } from '@clerk/nextjs'
+import { redirectToSignIn } from '@clerk/nextjs/server'
+import { and, eq } from 'drizzle-orm'
+import { redirect } from 'next/navigation'
 
-function ServerIdPage() {
-  return <div>ServerPage</div>
+interface ServerIdPageProps {
+  params: { serverId: string }
+}
+
+async function ServerIdPage({ params }: ServerIdPageProps) {
+  const user = await currentUser()
+
+  if (!user) {
+    return redirectToSignIn()
+  }
+
+  const generalChannel = await db
+    .select()
+    .from(Channel)
+    .where(and(eq(Channel.serverId, params.serverId), eq(Channel.name, 'general')))
+    .then(res => res[0])
+
+  if (!generalChannel) {
+    return null
+  }
+
+  return redirect(`/servers/${params.serverId}/channels/${generalChannel.id}`)
 }
 
 export default ServerIdPage
