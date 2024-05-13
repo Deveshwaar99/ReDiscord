defac6be429ed34794ad204c8a3ba37081f1b4eb
Deveshwaar <deveshwaar99@gmail.com>
2024-05-13 13:31:13 +0530

    ui : Add Channel Id page

--- src/app/(root)/servers/[serverId]/channels/[channelId]/page.tsx
+++ src/app/(root)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,45 @@
+import ChatHeader from '@/components/chat/ChatHeader'
+import { db } from '@/db'
+import { Channel, Member } from '@/db/schema'
+import { getProfile } from '@/lib/getProfile'
+import { redirectToSignIn } from '@clerk/nextjs'
+import { and, eq } from 'drizzle-orm'
+import { redirect } from 'next/navigation'
 
-function ChannelIdPage() {
+interface ChannelIdProps {
+  params: {
+    serverId: string
+    channelId: string
+  }
+}
+
+async function ChannelIdPage({ params }: ChannelIdProps) {
+  const profile = await getProfile()
+  if (!profile) {
+    return redirectToSignIn()
+  }
+
+  const channel = await db
+    .select()
+    .from(Channel)
+    .where(and(eq(Channel.serverId, params.serverId), eq(Channel.id, params.channelId)))
+    .then(res => res[0])
+
+  const member = await db
+    .select()
+    .from(Member)
+    .where(and(eq(Member.serverId, params.serverId), eq(Member.profileId, profile.id)))
+    .then(res => res[0])
+
+  if (!channel || !member) {
+    redirect('/')
+  }
+
   return (
-    <div>ChannelIdPage</div>
+    <div className="flex h-full flex-col bg-white dark:bg-[#313338]">
+      <ChatHeader serverId={params.serverId} name={channel.name} type="channel" />
+      ChannelIdPage
+    </div>
   )
 }
 
