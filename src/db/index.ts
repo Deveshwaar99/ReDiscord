585a499b6c025f7316779aa68aa708c0c262aecc
Deveshwaar <deveshwaar99@gmail.com>
2024-05-15 18:21:06 +0530

    chore(db): remove comments

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,11 +3,13 @@ import 'server-only'
 import * as schema from '@/db/schema'
 import { Pool } from '@neondatabase/serverless'
 import { drizzle } from 'drizzle-orm/neon-serverless'
-// let dbInstance: NeonHttpDatabase<Record<string, never>>
 
-// if (!dbInstance) {
+// pool cooenction
 const pool = new Pool({ connectionString: process.env.DATABASE_URL })
 let dbInstance = drizzle(pool, { schema })
-// }
 
 export { dbInstance as db }
+
+//http connection
+// const sql = neon(process.env.DRIZZLE_DATABASE_URL!)
+// const db = drizzle(sql, { schema })
