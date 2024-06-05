ba79861472e9e6fc1fe7065f454c6723bb8e652b
Deveshwaar <deveshwaar99@gmail.com>
2024-06-05 12:50:05 +0530

    fix loading styles

--- src/app/(root)/servers/loading.tsx
+++ src/app/(root)/servers/loading.tsx
@@ -3,7 +3,7 @@ import LoadingSidebar from '@/components/LoadingSidebar'
 
 function LoadingServerPage() {
   return (
-    <div className="flex min-h-screen min-w-full items-center justify-center">
+    <div className="flex min-h-screen min-w-full">
       <LoadingNavigationBar />
       <LoadingSidebar />
       <main className="absolute left-1/2 top-1/2 h-full md:pl-[72px]">Loading...</main>
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import LoadingNavigationBar from '@/components/LoadingNavigationBar'
 
 function HomeLoadingPage() {
   return (
-    <div className="flex min-h-screen min-w-full items-center justify-center">
+    <div className="flex min-h-screen min-w-full">
       <LoadingNavigationBar />
       <main className="absolute left-1/2 top-1/2 h-full md:pl-[72px]">Loading...</main>
     </div>
