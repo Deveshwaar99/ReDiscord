bcd142640c9e31373aa222232c8e15da8379f3cd
Deveshwaar <deveshwaar99@gmail.com>
2024-06-16 10:59:35 +0530

    feat (get-profile):Add getProfile for pages endpoint

---
+++ src/lib/getProfile-pages.ts
@@ -0,0 +1,19 @@
+import { db } from '@/db/index'
+import { Profile, type SelectProfile } from '@/db/schema'
+import { getAuth } from '@clerk/nextjs/server'
+import { eq } from 'drizzle-orm'
+import type { NextApiRequest } from 'next'
+
+export async function getProfilePages(req: NextApiRequest): Promise<SelectProfile | null> {
+  const { userId } = getAuth(req)
+
+  if (!userId) return null
+
+  const profile = await db
+    .select()
+    .from(Profile)
+    .where(eq(Profile.clerkId, userId))
+    .then(res => res[0])
+
+  return profile
+}
