f9198deee5f222ff5d66c486bd36b61db975e4c0
Deveshwaar <deveshwaar99@gmail.com>
2024-06-16 11:07:59 +0530

    feat (db) : Create database connection for pages api

---
+++ src/db/pages-index.ts
@@ -0,0 +1,9 @@
+import * as schema from '@/db/schema'
+import { Pool } from '@neondatabase/serverless'
+import { drizzle } from 'drizzle-orm/neon-serverless'
+
+// pool cooenction
+const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+let dbInstance = drizzle(pool, { schema })
+
+export { dbInstance as db }
