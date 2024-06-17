c996d9c7c3a88ccb10fa2723fd197df86f37e1dc
Deveshwaar <deveshwaar99@gmail.com>
2024-06-17 12:01:20 +0530

    refactor : use unknown type instead of any

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,26 +6,24 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-const ServerDataSchema = z.object({
-  name: z.string().min(5).max(25),
-  imageUrl: z.string().min(1),
-})
-
-const ChannelDataSchema = z.object({
-  name: z
-    .string()
-    .min(5)
-    .max(15)
-    .refine(val => val !== 'general', {
-      message: "The name cannot be 'general'",
-    }),
-  type: z.enum(['AUDIO', 'TEXT', 'VIDEO']),
-})
-
-export function verifyServerData(requestData: any) {
+export function verifyServerData(requestData: unknown) {
+  const ServerDataSchema = z.object({
+    name: z.string().min(5).max(25),
+    imageUrl: z.string().min(1),
+  })
   return ServerDataSchema.safeParse(requestData)
 }
 
-export function verifyChannelData(requestData: any) {
+export function verifyChannelData(requestData: unknown) {
+  const ChannelDataSchema = z.object({
+    name: z
+      .string()
+      .min(5)
+      .max(15)
+      .refine(val => val !== 'general', {
+        message: "The name cannot be 'general'",
+      }),
+    type: z.enum(['AUDIO', 'TEXT', 'VIDEO']),
+  })
   return ChannelDataSchema.safeParse(requestData)
 }
