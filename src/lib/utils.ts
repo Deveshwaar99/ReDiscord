a42f9b09b4f0a1f2a87ee2a07762ed01bcb9d762
Deveshwaar <deveshwaar99@gmail.com>
2024-06-22 15:26:10 +0530

    feat : Add createMessageObject function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
-import { clsx, type ClassValue } from 'clsx'
+import type { SelectMember, SelectMessage, SelectProfile, selectDirectMessage } from '@/db/schema'
+import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 import { z } from 'zod'
+import { MemberRoles, type MessageWithMemberAndProfile } from '../../types'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -26,4 +28,27 @@ export function verifyChannelData(requestData: unknown) {
     type: z.enum(['AUDIO', 'TEXT', 'VIDEO']),
   })
   return ChannelDataSchema.safeParse(requestData)
+}
+
+export function createMessageObject({
+  message,
+  member,
+  profile,
+}: {
+  message: SelectMessage | selectDirectMessage
+  member: SelectMember
+  profile: SelectProfile
+}): MessageWithMemberAndProfile {
+  return {
+    messageId: message.id,
+    content: message.content,
+    fileUrl: message.fileUrl,
+    deleted: message.deleted,
+    createdAt: message.createdAt,
+    updatedAt: message.updatedAt,
+    memberId: member.id,
+    memberRole: MemberRoles[member.role],
+    profileName: profile.name,
+    profileImage: profile.imageUrl,
+  }
 }
