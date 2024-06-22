53c6d52c1aed5fde5fcf99aefbe7cc5ce9df66f8
Deveshwaar <deveshwaar99@gmail.com>
2024-06-22 16:21:11 +0530

    refactor(conversation): optimize getOrCreateConversation function

--- src/lib/conversations.ts
+++ src/lib/conversations.ts
@@ -2,12 +2,15 @@ import 'server-only'
 
 import { db } from '@/db'
 import { Conversation } from '@/db/schema'
-import { and, eq } from 'drizzle-orm'
+import { and, eq, or } from 'drizzle-orm'
 
 export async function getOrCreateConversation(memberOneId: string, memberTwoId: string) {
-  let conversation = await (findConversation(memberOneId, memberTwoId) ||
-    findConversation(memberTwoId, memberOneId))
-
+  let conversation = await db.query.Conversation.findFirst({
+    where: or(
+      and(eq(Conversation.memberOneId, memberOneId), eq(Conversation.memberTwoId, memberTwoId)),
+      and(eq(Conversation.memberOneId, memberTwoId), eq(Conversation.memberTwoId, memberOneId)),
+    ),
+  })
   if (!conversation) {
     conversation = await createConversation(memberOneId, memberTwoId)
   }
@@ -18,36 +21,9 @@ async function findConversation(memberOneId: string, memberTwoId: string) {
   const conversation = await db.query.Conversation.findFirst({
     where: and(
       eq(Conversation.memberOneId, memberOneId),
-      eq(Conversation.memberTwoId, memberTwoId)
+      eq(Conversation.memberTwoId, memberTwoId),
     ),
   })
-
-  // const MemberOne = alias(Member, 'memberOne')
-  // const MemberTwo = alias(Member, 'memberTwo')
-  // const ProfileOne = alias(Profile, 'profileOne')
-  // const ProfileTwo = alias(Profile, 'profileTwo')
-
-  // const conversation = await db
-  //   .select({
-  //     id: Conversation.id,
-  //     memberOneId: Conversation.memberOneId,
-  //     memberTwoId: Conversation.memberTwoId,
-  //     createdAt: Conversation.createdAt,
-  //     updatedAt: Conversation.updatedAt,
-  //     MemberOne,
-  //     MemberTwo,
-  //     ProfileOne,
-  //     ProfileTwo,
-  //   })
-  //   .from(Conversation)
-  //   .leftJoin(MemberOne, eq(Conversation.memberOneId, Member.id))
-  //   .leftJoin(MemberTwo, eq(Conversation.memberTwoId, Member.id))
-  //   .leftJoin(ProfileOne, eq(MemberOne.id, Profile.id))
-  //   .leftJoin(ProfileTwo, eq(MemberTwo.id, Profile.id))
-  //   .where(
-  //     and(eq(Conversation.memberOneId, memberOneId), eq(Conversation.memberTwoId, memberTwoId))
-  //   )
-  //   .then(res => res[0])
 
   return conversation
 }
