d4364af4cf81670e19d14e0a0e1d83bc77fb1849
Deveshwaar <deveshwaar99@gmail.com>
2024-06-23 17:33:33 +0530

    ui(media-romm): Add MediaRoom Componet

---
+++ src/components/MediaRoom.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+interface MediaRoomProps {
+  chatId: string
+  audio: boolean
+  video: boolean
+}
+function MediaRoom() {
+  return <div>MediaRoom</div>
+}
+
+export default MediaRoom
