d689e052b025ef040a07bcdf9f4d9dcf3406d308
Deveshwaar <deveshwaar99@gmail.com>
2024-06-25 19:31:36 +0530

    ui (auth) : Add bg for auth pages

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import Image from 'next/image'
+import type React from 'react'
 
 function AuthLayout({ children }: { children: React.ReactNode }) {
-  return <div className=" flex items-center justify-center h-full bg-white ">{children}</div>
+  return (
+    <div className="flex h-screen w-full">
+      <div className="relative w-7/12">
+        <Image src="/auth.svg" alt="Auth" fill className="absolute inset-0 object-cover" priority />
+      </div>
+      <div className="flex w-5/12 items-center justify-center p-8">{children}</div>
+    </div>
+  )
 }
 
 export default AuthLayout
