41e89f88e379e14ff711f538422a2d8480418439
Deveshwaar <deveshwaar99@gmail.com>
2024-08-29 22:27:00 +0530

    refactor(middleware): update to clerkMiddleware and createRouteMatcher
    Replaced authMiddleware with clerkMiddleware and implemented createRouteMatcher for handling public routes. Updated matcher configuration to improve route handling and skip static files and Next.js internals.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,21 @@
-import { authMiddleware } from '@clerk/nextjs'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export default authMiddleware({
-  publicRoutes: ['/api/uploadthing'],
-})
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+
+export default clerkMiddleware(
+  (auth, request) => {
+    if (!isPublicRoute(request)) {
+      auth().protect()
+    }
+  }
+  // { debug: process.env.NODE_ENV === 'development' }
+)
 
 export const config = {
   matcher: [
-    // Exclude files with a "." followed by an extension, which are typically static files.
-    // Exclude files in the _next directory, which are Next.js internals.
-    '/((?!.+\\.[\\w]+$|_next).*)',
-    // Re-include any files in the api or trpc folders that might have an extension
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 }
