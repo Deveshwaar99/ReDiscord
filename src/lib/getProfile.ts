9a1cd02a436df3536d731d641f9c3bc17d4c9b50
Deveshwaar <deveshwaar99@gmail.com>
2024-08-29 22:30:56 +0530

    refactor(clerk): update auth import to use server-specific module

--- src/lib/getProfile.ts
+++ src/lib/getProfile.ts
@@ -2,13 +2,12 @@ import 'server-only'
 
 import { db } from '@/db'
 import { Profile, type SelectProfile } from '@/db/schema'
-import { auth } from '@clerk/nextjs'
+import { auth } from '@clerk/nextjs/server'
 import { eq } from 'drizzle-orm'
 import { cache } from 'react'
 
 export const getProfile = cache(async (): Promise<SelectProfile | null> => {
   const { userId } = auth()
-
   if (!userId) return null
 
   return await db
