22bd7b4da958a7ad44e305bde5b2f7a79ac07687
Deveshwaar <deveshwaar99@gmail.com>
2024-08-29 22:30:14 +0530

    fix(initial-profile): Create a profile if no profiles are found in the database

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,8 +1,10 @@
 import NavigationSidebar from '@/components/navigation/NavigationSidebar'
+import { initialProfile } from '@/lib/initialProfile'
 
 import type { PropsWithChildren } from 'react'
 
 async function MainLayout({ children }: PropsWithChildren) {
+  const profile = await initialProfile()
   return (
     <div className="h-full">
       <div className="fixed inset-y-0 z-30 hidden h-full w-[72px] flex-col md:flex">
--- src/lib/initialProfile.ts
+++ src/lib/initialProfile.ts
@@ -2,16 +2,22 @@ import 'server-only'
 
 import { db } from '@/db'
 import { Profile, type SelectProfile } from '@/db/schema'
-import { currentUser, redirectToSignIn } from '@clerk/nextjs'
+
+import { auth, currentUser } from '@clerk/nextjs/server'
 import { eq } from 'drizzle-orm'
 
 export async function initialProfile(): Promise<SelectProfile> {
   const user = await currentUser()
   if (!user) {
-    return redirectToSignIn()
+    return auth().redirectToSignIn()
   }
 
-  const [profile] = await db.select().from(Profile).where(eq(Profile.clerkId, user.id))
+  const profile = await db
+    .select()
+    .from(Profile)
+    .where(eq(Profile.clerkId, user.id))
+    .then(res => res[0])
+
   if (profile) {
     return profile
   }
